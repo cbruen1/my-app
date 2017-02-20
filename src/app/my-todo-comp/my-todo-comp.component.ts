741b03e0df2d7172591f40b3b644260013e11de0
Ciaran Bruen <cbruen1@hotmail.com>
2017-02-20 12:09:53 +0100

    Add new component

---
+++ src/app/my-todo-comp/my-todo-comp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-todo-comp',
+  templateUrl: './my-todo-comp.component.html',
+  styleUrls: ['./my-todo-comp.component.css']
+})
+export class MyTodoCompComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
